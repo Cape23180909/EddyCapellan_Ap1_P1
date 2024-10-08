b7b8b38be910c86c196e3f61c160cfcdf6beae46
Cape23180909 <samilgamer@outlook.com>
2024-10-08 01:32:36 -0400

    Terminado de hacer el modelo Deudores para dicho sea seleccionado con un select.

---
+++ Models/Deudores.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EddyCapellan_Ap1_P1.Models;
+
+public class Deudores
+{
+    [Key]
+    public int DeudorId { get; set; }
+    [Required(ErrorMessage = "Favor colocar un nombre.")]
+    public string Nombres { get; set; }
+}
