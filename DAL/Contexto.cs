6e26c4300e2cf6c5886fa515949973d7a71e17f2
Cape23180909 <samilgamer@outlook.com>
2024-10-08 01:26:07 -0400

    Injectando el contexto del nuevo modelo "Deudores" y colocando el OnModekCreation para seleccionar dicho deudor en este caso

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,10 +3,19 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 namespace EddyCapellan_Ap1_P1.DAL;
-
-    public class Contexto :DbContext
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options)
+        : base(options) { }
+    public DbSet<Prestamos> Prestamos { get; set; }
+    public DbSet<Deudores> Deudores { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-
-    public DbSet<Prestamos> Prestamos{ get; set; }
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Deudores>().HasData(new List<Deudores>()
+        {
+            new Deudores() {DeudorId= 1, Nombres="Samil"},
+             new Deudores { DeudorId = 2, Nombres = "Maria" }
+        });
+    }
 }
