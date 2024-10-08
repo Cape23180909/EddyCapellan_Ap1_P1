e481dfbe63c115197a72d450c924cbca7adc34e0
Cape23180909 <samilgamer@outlook.com>
2024-10-08 01:34:05 -0400

    Colocando las validaciones y las nuevas propiedades que es balance y su llave foranea de lugar para obtener las propiedades de dicha modelo que es deudores que handa ahien el dicho proyecto.

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EddyCapellan_Ap1_P1.Models;
 
@@ -6,11 +7,19 @@ public class Prestamos
 {
     [Key]
     public int PrestamoId { get; set; }
-    [Required(ErrorMessage = "Favor colocar el nombre del deudor.")]
-    public string? Deudor { get; set; }
+    [Required(ErrorMessage = "Favor seleccionar un deudor.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un deudor válido.")]
+    public int DeudorId { get; set; }
+    [ForeignKey("DeudorId")]
+    public Deudores Deudor { get; set; }
+
     [Required(ErrorMessage = "Favor colocar el concepto.")]
     public string? Concepto { get; set; }
     [Required(ErrorMessage = "Favor colocar el monto.")]
     public decimal? Monto { get; set; }
+    [Required(ErrorMessage = "Favor colocar el balance.")]
+    public decimal? Balance { get; set; }
 
+    [ForeignKey("PrestamoId")]
+    public ICollection<Deudores> Deudores { get; set; } = new List<Deudores>();
 }
