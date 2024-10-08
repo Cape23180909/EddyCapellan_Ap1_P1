f73fc96d5a1bfa75f4b8aecc45ea949ea97b94dd
Cape23180909 <samilgamer@outlook.com>
2024-10-08 11:21:35 -0400

    Alimentando a DbContext para el modelo "Cobros"

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -9,13 +9,14 @@ public class Contexto : DbContext
         : base(options) { }
     public DbSet<Prestamos> Prestamos { get; set; }
     public DbSet<Deudores> Deudores { get; set; }
+    public DbSet<Cobros> Cobros { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Deudores>().HasData(new List<Deudores>()
         {
             new Deudores() {DeudorId= 1, Nombres="Samil"},
-             new Deudores { DeudorId = 2, Nombres = "Maria" }
+            new Deudores { DeudorId = 2, Nombres = "Maria" }
         });
     }
 }
