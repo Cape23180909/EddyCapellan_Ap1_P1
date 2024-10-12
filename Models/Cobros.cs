1b98badc7a9dc9d9df070faf2b94feea894417e9
Cape23180909 <samilgamer@outlook.com>
2024-10-12 01:16:52 -0400

    Terminando de hacer el modelo de cobros.

---
+++ Models/Cobros.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EddyCapellan_Ap1_P1.Models;
+public class Cobros
+{
+    [Key]
+    public int CobroId { get; set; }
+    [Required(ErrorMessage ="Favor colocar una fecha.")]
+    public DateTime? Fecha { get; set; }
+    [Required(ErrorMessage = "Favor seleccionar un deudor.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Favor seleccionar un deudor válido.")]
+    public int DeudorId { get; set; }
+    [ForeignKey("DeudorId")]
+    public Deudores Deudor { get; set; }
+
+    [Required(ErrorMessage = "Favor colocar el monto del cobro.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor que cero.")]
+    public decimal Monto { get; set; }
+
+    public ICollection<CobroDetalle> CobroDetalles { get; set; } = new List<CobroDetalle>();
+}
