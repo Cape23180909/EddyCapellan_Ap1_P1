693ddcb130a4e1481c9781264b8a65f0d98f8332
Cape23180909 <samilgamer@outlook.com>
2024-10-12 01:17:12 -0400

    Terminando de hacer el modelo de cobrodetalle.

---
+++ Models/CobroDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EddyCapellan_Ap1_P1.Models;
+
+public class CobroDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    [Required(ErrorMessage = "Favor seleccionar un cobro.")]
+    public int CobroId { get; set; }
+    [ForeignKey("CobroId")]
+    public Cobros Cobro { get; set; }
+
+    [Required(ErrorMessage = "Favor seleccionar un préstamo.")]
+    public int PrestamoId { get; set; }
+    [ForeignKey("PrestamoId")]
+    public Prestamos Prestamo { get; set; }
+
+    [Required(ErrorMessage = "Favor colocar el valor cobrado.")]
+    public decimal ValorCobrado { get; set; }
+
+}
